329689c1a359980c04999cb44b36dc6003f6e032
GizG <57505041+eatonbla5678@users.noreply.github.com>
2024-12-18 10:56:54 -0600

    Update RobotCode.java
    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/RobotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/RobotCode.java
@@ -52,64 +52,64 @@ public class RobotCode extends OpMode {
     }
 
     public void drive() {
-        double y = -gamepad1.left_stick_y; // This is reversed
-        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
-        double z = gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y; // This is reversed
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double z = gamepad1.right_stick_x;
 
-        double leftFrontPower = y + x + z;
-        double leftRearPower = y - x + z;
-        double rightFrontPower = y - x - z;
-        double rightRearPower = y + x - z;
+            double leftFrontPower = y + x + z;
+            double leftRearPower = y - x + z;
+            double rightFrontPower = y - x - z;
+            double rightRearPower = y + x - z;
 
-        if (Math.abs(leftFrontPower) > 1 || Math.abs(leftRearPower) > 1 ||
-                Math.abs(rightFrontPower) > 1 || Math.abs(rightRearPower) > 1) {
-            // Find the largest power
-            double max;
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(leftRearPower));
-            max = Math.max(Math.abs(rightFrontPower), max);
-            max = Math.max(Math.abs(rightRearPower), max);
+            if (Math.abs(leftFrontPower) > 1 || Math.abs(leftRearPower) > 1 ||
+                    Math.abs(rightFrontPower) > 1 || Math.abs(rightRearPower) > 1) {
+                // Find the largest power
+                double max;
+                max = Math.max(Math.abs(leftFrontPower), Math.abs(leftRearPower));
+                max = Math.max(Math.abs(rightFrontPower), max);
+                max = Math.max(Math.abs(rightRearPower), max);
 
-            // Everything is Positive, do not worry about signs
-            leftFrontPower /= max;
-            leftRearPower /= max;
-            rightFrontPower /= max;
-            rightRearPower /= max;
-        }
+                // Everything is Positive, do not worry about signs
+                leftFrontPower /= max;
+                leftRearPower /= max;
+                rightFrontPower /= max;
+                rightRearPower /= max;
+            }
 
-        if (gamepad1.dpad_left) {
-            leftFrontPower = -1;
-            rightRearPower = -1;
-            leftRearPower = 1;
-            rightFrontPower = 1;
-        } else if (gamepad1.dpad_right) {
-            leftFrontPower = 1;
-            rightRearPower = 1;
-            leftRearPower = -1;
-            rightFrontPower = -1;
-        } else if (gamepad1.dpad_up) {
-            leftFrontPower = 1;
-            rightRearPower = 1;
-            leftRearPower = 1;
-            rightFrontPower = 1;
-        } else if (gamepad1.dpad_down) {
-            leftFrontPower = -1;
-            leftRearPower = -1;
-            rightRearPower = -1;
-            rightFrontPower = -1;
-        }
+            if (gamepad1.dpad_left) {
+                leftFrontPower = -1;
+                rightRearPower = -1;
+                leftRearPower = 1;
+                rightFrontPower = 1;
+            } else if (gamepad1.dpad_right) {
+                leftFrontPower = 1;
+                rightRearPower = 1;
+                leftRearPower = -1;
+                rightFrontPower = -1;
+            } else if (gamepad1.dpad_up) {
+                leftFrontPower = 1;
+                rightRearPower = 1;
+                leftRearPower = 1;
+                rightFrontPower = 1;
+            } else if (gamepad1.dpad_down) {
+                leftFrontPower = -1;
+                leftRearPower = -1;
+                rightRearPower = -1;
+                rightFrontPower = -1;
+            }
 
-        if (gamepad1.left_bumper && currentMode == FAST_MODE && buttonTime.time() >= 500) {
-            currentMode = SLOW_MODE;
-            buttonTime.reset();
-        } else if (gamepad1.left_bumper && currentMode == SLOW_MODE && buttonTime.time() >= 500) {
-            currentMode = FAST_MODE;
-            buttonTime.reset();
-        }
+            if (gamepad1.left_bumper && currentMode == FAST_MODE && buttonTime.time() >= 500) {
+                currentMode = SLOW_MODE;
+                buttonTime.reset();
+            } else if (gamepad1.left_bumper && currentMode == SLOW_MODE && buttonTime.time() >= 500) {
+                currentMode = FAST_MODE;
+                buttonTime.reset();
+            }
 
-        hardware.frontLeft.setPower(leftFrontPower * currentMode);
-        hardware.rearLeft.setPower(leftRearPower * currentMode);
-        hardware.frontRight.setPower(rightFrontPower * currentMode);
-        hardware.rearRight.setPower(rightRearPower * currentMode);
+            hardware.frontLeft.setPower(leftFrontPower * currentMode);
+            hardware.rearLeft.setPower(leftRearPower * currentMode);
+            hardware.frontRight.setPower(rightFrontPower * currentMode);
+            hardware.rearRight.setPower(rightRearPower * currentMode);
     }
 
     public void intake() {
@@ -151,7 +151,7 @@ public class RobotCode extends OpMode {
                 hardware.leftServo.setPosition(0);
                 telemetry.addData("Flaps: ", "Open");
             } else {
-                hardware.rightServo.setPosition(0.1);
+                hardware.rightServo.setPosition(0);
                 hardware.leftServo.setPosition(0.5);
                 telemetry.addData("Flaps: ", "Closed");
             }
